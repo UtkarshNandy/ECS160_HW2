4e4fef274eb029cde985bec5992cc0a9c39a0c83
Denil Neil <denilcollins@gmail.com>
2025-02-22 20:59:02 -0800

    fix session class, add lazyload interface

--- src/main/java/com/ecs160/persistence/Session.java
+++ src/main/java/com/ecs160/persistence/Session.java
@@ -23,179 +23,237 @@ import redis.clients.jedis.Jedis;
 
 // Assumption - only support int/long/and string values
 public class Session {
-
-    private List<Object> objectList = new ArrayList<>();
-    private Jedis jedisSession;
+    private List<Object> pendingObjects = new ArrayList<>();
+    private Jedis redisConnection;
 
     public Session() {
-        jedisSession = new Jedis("localhost", 6379);
+        redisConnection = new Jedis("localhost", 6379);
     }
-
 
     public void add(Object obj) {
-        objectList.add(obj);
+        pendingObjects.add(obj);
     }
 
-    public void persistAll() {
-        if (objectList.isEmpty()) {
+    private String extractIdentifier(Object obj, Field field) throws IllegalAccessException {
+        field.setAccessible(true);
+        Object idValue = field.get(obj);
+        return idValue != null ? idValue.toString() : "";
+    }
+
+    private Map<String, String> buildEntityData(Object obj, Class<?> objClass) throws IllegalAccessException {
+        Map<String, String> data = new HashMap<>();
+
+        for (Field field : objClass.getDeclaredFields()) {
+            field.setAccessible(true);
+
+            if (field.isAnnotationPresent(PersistableField.class)) {
+                Object value = field.get(obj);
+                if (value != null) {
+                    data.put(field.getName(), value.toString());
+                }
+            }
+        }
+
+        return data;
+    }
+
+    private void persistListField(Object obj, Field listField, Map<String, String> parentData) throws IllegalAccessException {
+        listField.setAccessible(true);
+        Object listValue = listField.get(obj);
+
+        if (!(listValue instanceof List<?>)) {
             return;
         }
 
-        for (Object obj : objectList) {
-            Class<?> clazz = obj.getClass();
+        List<?> elements = (List<?>) listValue;
+        List<String> elementIds = new ArrayList<>();
 
-            // skip if not persistable
-            if (!clazz.isAnnotationPresent(Persistable.class)) {
+        for (Object element : elements) {
+            if (!element.getClass().isAnnotationPresent(Persistable.class)) {
                 continue;
             }
 
-            String redisKey = "";
-            Map<String, String> redisData = new HashMap<>();
+            String elementId = "";
+            Map<String, String> elementData = new HashMap<>();
 
-            for (Field field : clazz.getDeclaredFields()) {
-                field.setAccessible(true);
-                try {
-                    if (field.isAnnotationPresent(PersistableId.class)) {
-                        Object id = field.get(obj); // get ID
-                        if (id != null) {
-                            redisKey = id.toString();
-                            redisData.put(field.getName(), redisKey);
-                        }
+            for (Field elementField : element.getClass().getDeclaredFields()) {
+                elementField.setAccessible(true);
+
+                if (elementField.isAnnotationPresent(PersistableId.class)) {
+                    elementId = extractIdentifier(element, elementField);
+                    elementData.put(elementField.getName(), elementId);
+                } else if (elementField.isAnnotationPresent(PersistableField.class)) {
+                    Object fieldValue = elementField.get(element);
+                    if (fieldValue != null) {
+                        elementData.put(elementField.getName(), fieldValue.toString());
                     }
-                    // Persist other fields
-                    if (field.isAnnotationPresent(PersistableField.class)) {
-                        Object value = field.get(obj);
-                        if (value != null) {
-                            redisData.put(field.getName(), value.toString());
-                        }
-                    }
-                    // Persist list fields
-                    if (field.isAnnotationPresent(PersistableListField.class)) {
-                        Object listObj = field.get(obj);
-                        if (listObj instanceof List<?>) {
-                            List<?> list = (List<?>) listObj;
-                            List<String> idList = new ArrayList<>();
-                            for (Object element : list) {
-                                Class<?> elemClass = element.getClass();
-                                if (!elemClass.isAnnotationPresent(Persistable.class)) {
-                                    continue;
-                                }
-                                String childRedisKey = "";
-                                Map<String, String> childData = new HashMap<>();
-                                for (Field elementField : elemClass.getDeclaredFields()) {
-                                    elementField.setAccessible(true);
-                                    if (elementField.isAnnotationPresent(PersistableId.class)) {
-                                        Object childId = elementField.get(element);
-                                        if (childId != null) {
-                                            childRedisKey = childId.toString();
-                                            childData.put(elementField.getName(), childRedisKey);
-                                        }
-                                    }
-                                    if (elementField.isAnnotationPresent(PersistableField.class)) {
-                                        Object childValue = elementField.get(element);
-                                        if (childValue != null) {
-                                            childData.put(elementField.getName(), childValue.toString());
-                                        }
-                                    }
-                                }
-                                if (!childRedisKey.isEmpty()) {
-                                    jedisSession.hmset(childRedisKey, childData);
-                                    idList.add(childRedisKey);
-                                }
-                            }
-                            redisData.put(field.getName(), String.join(",", idList));
-                        }
-                    }
-                } catch (IllegalAccessException e) {
-                    e.printStackTrace();
                 }
             }
-            if (!redisKey.isEmpty()) {
-                jedisSession.hmset(redisKey, redisData);
-            } else {
+
+            if (!elementId.isEmpty()) {
+                redisConnection.hmset(elementId, elementData);
+                elementIds.add(elementId);
             }
         }
-        objectList.clear();
+
+        if (!elementIds.isEmpty()) {
+            parentData.put(listField.getName(), String.join(",", elementIds));
+        }
     }
 
+    public void persistAll() {
+        if (pendingObjects.isEmpty()) {
+            return;
+        }
 
+        for (Object obj : pendingObjects) {
+            Class<?> objClass = obj.getClass();
 
+            if (!objClass.isAnnotationPresent(Persistable.class)) {
+                continue;
+            }
 
+            try {
+                String objectId = "";
+                Map<String, String> objectData = new HashMap<>();
 
-    public Object load(Object object)  {
-        Class<?> clazz = object.getClass();
-        Field id = null;
+                // First, process the ID field
+                for (Field field : objClass.getDeclaredFields()) {
+                    if (field.isAnnotationPresent(PersistableId.class)) {
+                        objectId = extractIdentifier(obj, field);
+                        objectData.put(field.getName(), objectId);
+                        break;
+                    }
+                }
 
-        for(Field field: clazz.getDeclaredFields()){
-            if(field.isAnnotationPresent(PersistableId.class)){
-                id = field;
+                if (objectId.isEmpty()) {
+                    continue;
+                }
+
+                // Add regular persistable fields
+                objectData.putAll(buildEntityData(obj, objClass));
+
+                // Process list fields
+                for (Field field : objClass.getDeclaredFields()) {
+                    if (field.isAnnotationPresent(PersistableListField.class)) {
+                        persistListField(obj, field, objectData);
+                    }
+                }
+
+                redisConnection.hmset(objectId, objectData);
+
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
+        }
+
+        pendingObjects.clear();
+    }
+
+    private Object createPartialObject(Class<?> objClass, String id) throws Exception {
+        Object partialObj = objClass.getDeclaredConstructor().newInstance();
+
+        for (Field field : objClass.getDeclaredFields()) {
+            if (field.isAnnotationPresent(PersistableId.class)) {
+                field.setAccessible(true);
+                if (field.getType().equals(String.class)) {
+                    field.set(partialObj, id);
+                } else if (field.getType().equals(int.class) || field.getType().equals(Integer.class)) {
+                    field.set(partialObj, Integer.parseInt(id));
+                }
                 break;
             }
         }
-        if(id == null){
+
+        return partialObj;
+    }
+
+    private void loadField(Object obj, Field field, String value) throws IllegalAccessException {
+        field.setAccessible(true);
+        if (field.getType().equals(String.class)) {
+            field.set(obj, value);
+        } else if (field.getType().equals(int.class) || field.getType().equals(Integer.class)) {
+            field.set(obj, Integer.parseInt(value));
+        }
+    }
+
+    private List<Object> loadListField(String listValue, PersistableListField annotation) throws Exception {
+        if (listValue == null || listValue.isEmpty()) {
             return null;
         }
-        try{
-            id.setAccessible(true);
-            Object idVal = id.get(object);
-            if(idVal == null){
-                return null;
+
+        List<Object> loadedList = new ArrayList<>();
+        String[] elementIds = listValue.split(",");
+        Class<?> elementClass = Class.forName(annotation.className());
+
+        for (String elementId : elementIds) {
+            Object partialElement = createPartialObject(elementClass, elementId);
+            Object loadedElement = load(partialElement);
+            if (loadedElement != null) {
+                loadedList.add(loadedElement);
             }
-            String redisKey = idVal.toString();
-
-            Map<String, String> redisData = jedisSession.hgetAll(redisKey);
-            if(redisData == null || redisData.isEmpty()){
-                return null;
-            }
-
-            Object post = clazz.getDeclaredConstructor().newInstance();
-
-            for(Field field: clazz.getDeclaredFields()){
-                field.setAccessible(true);
-
-                if(field.isAnnotationPresent(PersistableId.class) || field.isAnnotationPresent(PersistableField.class) && redisData.containsKey(field.getName())){
-                    String value = redisData.get(field.getName());
-                    if(field.getType().equals(String.class)){
-                        field.set(post, value);
-                    } else if (field.getType().equals(int.class) || field.getType().equals(Integer.class)){
-                        field.set(post, Integer.parseInt(value));
-                    }
-                }
-
-                if(field.isAnnotationPresent(PersistableListField.class)){
-                    String listValue = redisData.get(field.getName());
-                    if(listValue != null && !listValue.isEmpty()){
-                        String[] childIds = listValue.split(",");
-                        List<Object> childList = new ArrayList<>();
-                        PersistableListField listAnnotation = field.getAnnotation(PersistableListField.class);
-                        String childClassName = listAnnotation.className();
-                        Class<?> childClass = Class.forName(childClassName);
-                        for(String childId : childIds){
-                            Object partChild = childClass.getDeclaredConstructor().newInstance();
-                            for(Field childField : childClass.getDeclaredFields()){
-                                if(childField.isAnnotationPresent(PersistableId.class)){
-                                    childField.setAccessible(true);
-                                    if(childField.getType().equals(String.class)){
-                                        childField.set(partChild, childId);
-                                    } else if(childField.getType().equals(int.class) || childField.getType().equals(Integer.class)) {
-                                        childField.set(partChild, Integer.parseInt(childId));
-                                    }
-                                    break;
-                                }
-                            }
-                            Object fullChild = load(partChild);
-                            if(fullChild != null){
-                                childList.add(fullChild);
-                            }
-                        }
-                        field.set(post, childList);
-                    }
-                }
-            }
-            return post;
-        } catch (Exception e){
-            e.printStackTrace();
         }
-        return null;
+
+        return loadedList;
+    }
+
+    public Object load(Object prototype) {
+        Class<?> objClass = prototype.getClass();
+
+        try {
+            // Find and validate ID field
+            Field idField = null;
+            for (Field field : objClass.getDeclaredFields()) {
+                if (field.isAnnotationPresent(PersistableId.class)) {
+                    idField = field;
+                    break;
+                }
+            }
+
+            if (idField == null) {
+                return null;
+            }
+
+            // Get object ID and fetch data
+            idField.setAccessible(true);
+            Object idValue = idField.get(prototype);
+            if (idValue == null) {
+                return null;
+            }
+
+            String redisKey = idValue.toString();
+            Map<String, String> redisData = redisConnection.hgetAll(redisKey);
+
+            if (redisData == null || redisData.isEmpty()) {
+                return null;
+            }
+
+            // Create and populate new object
+            Object loadedObject = objClass.getDeclaredConstructor().newInstance();
+
+            for (Field field : objClass.getDeclaredFields()) {
+                String storedValue = redisData.get(field.getName());
+
+                if (field.isAnnotationPresent(PersistableId.class) ||
+                        field.isAnnotationPresent(PersistableField.class)) {
+                    if (storedValue != null) {
+                        loadField(loadedObject, field, storedValue);
+                    }
+                } else if (field.isAnnotationPresent(PersistableListField.class)) {
+                    PersistableListField listAnnotation = field.getAnnotation(PersistableListField.class);
+                    List<Object> loadedList = loadListField(storedValue, listAnnotation);
+                    if (loadedList != null) {
+                        field.setAccessible(true);
+                        field.set(loadedObject, loadedList);
+                    }
+                }
+            }
+
+            return loadedObject;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
